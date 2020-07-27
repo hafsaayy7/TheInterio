84214d26aaf971e642310c8f86eb400e04c0d630
--replace-all <shahidbangashsi94@gmail.com>
2020-07-28 02:23:55 +0500

    sign up and sign form calidated ... by default sign in or sign up button will be disabled .. it will be automatically enabled once validaton is done ... after login and sign up willl be done in morning

---
+++ signup.php
@@ -0,0 +1,85 @@
+<?php
+
+$users = array(
+    array(
+        "user_name" =>"shahidbangash" ,
+        "email" => "shahid@gmail.com" ,
+        "password" => "12345678" ,
+        "first_name"=>"Shahid" ,
+        "last_name" => "Khan" ,
+        "cnic" => "14142421412123412" ,
+        "age" => "21",
+        "relationship_status" =>"single" ,
+        "gender"=>"male",
+        "address" => "dhjashdkjs hsdjkashd" ,
+        "permanent_address" => "djashdkjsa sjdhaks" ,
+        "telephone" => "13123213123" , 
+        "fax_number" => "2321312312",
+        "mobile_number" => "1312321321321" ,
+        "website_url" => "wwww.example.com/"),
+        
+        array(
+            "user_name" =>"hafsahafsa" ,
+            "email" => "hafsa@gmail.com" ,
+            "password" => "12345678" ,
+            "first_name"=>"hafsa" ,
+            "last_name" => "Khan" ,
+            "cnic" => "14142421412123412" ,
+            "age" => "21",
+            "relationship_status" =>"single" ,
+            "gender"=>"female",
+            "address" => "dhjashdkjs hsdjkashd" ,
+            "permanent_address" => "djashdkjsa sjdhaks" ,
+            "telephone" => "13123213123" , 
+            "fax_number" => "2321312312",
+            "mobile_number" => "1312321321321" ,
+            "website_url" => "wwww.example.com/"
+        ),
+        
+    );
+
+    // replace the above code with database ... above data is from previous assignment
+
+    function search_username($id, $array) {
+        foreach ($array as $key => $val) {
+            if ($val['user_name'] === $id) {
+                return "true";
+            }
+        }
+        return "false";
+    }
+
+    
+    if (isset($_REQUEST['submit'])){
+
+    echo($username = $_REQUEST['usernamefield']);
+    $email = $_REQUEST['emailfield'];
+    $password = $_REQUEST['password'];
+    $first_name = $_REQUEST['firstnamefield'];
+    $last_name = $_REQUEST['lastnamefield'];
+
+    }
+    else{
+        echo 'Sign up not successful';
+
+        
+    }
+    // $cnic = $_REQUEST['cnic'];
+    // $age = $_REQUEST['age'];
+    // $relationship_status = $_REQUEST['relationship'];
+    // $gender = $_REQUEST['gender'];
+    // $address = $_REQUEST['mailingfield'];
+    // $permanent_address = $_REQUEST['addressfield'];
+    // $telephone = $_REQUEST['Telephonefield'];
+    // $fax_number = $_REQUEST['Faxfield'];
+    // $mobile_number = $_REQUEST['mobilefield'];
+    // $website_url = $_REQUEST['websitefield'];
+
+    
+
+
+
+
+
+
+?>
